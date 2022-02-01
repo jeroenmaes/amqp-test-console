b130ddb61b6ef829f15462fc0993691e91fe548c
Jeroen Maes <jeroen.maes@cegeka.com>
2022-02-01 11:09:38 +0100

    Print TLS + NET version

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Configuration;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace AmqpTestConsole
@@ -12,6 +14,9 @@ namespace AmqpTestConsole
         {
             try
             {
+                GetNetVersion();
+                SetHighestTlsVersion();
+
                 settings = new ConnectionSettings
                 {
                     Server = ConfigurationManager.AppSettings["connection"],
@@ -26,14 +31,99 @@ namespace AmqpTestConsole
             catch (Exception e)
             {
                 Console.WriteLine("Unexpected Exception: " + e.Message);
-                
+                if (e.InnerException != null)
+                    Console.WriteLine("Inner Exception: " + e.InnerException.Message);
+
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadKey();
+            }
+        }
+
+        private static void GetNetVersion()
+        {
+            const string subkey = @"SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\";
+
+            using (var ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32).OpenSubKey(subkey))
+            {
+                if (ndpKey != null && ndpKey.GetValue("Release") != null)
+                {
+                    Console.WriteLine($".NET Framework Version: {CheckFor45PlusVersion((int)ndpKey.GetValue("Release"))}");
+                }
+                else
+                {
+                    Console.WriteLine(".NET Framework Version 4.5 or later is not detected.");
+                }
             }            
+        }                
+        private static string CheckFor45PlusVersion(int releaseKey)
+        {
+            if (releaseKey >= 528040)
+                return "4.8 or later";
+            if (releaseKey >= 461808)
+                return "4.7.2";
+            if (releaseKey >= 461308)
+                return "4.7.1";
+            if (releaseKey >= 460798)
+                return "4.7";
+            if (releaseKey >= 394802)
+                return "4.6.2";
+            if (releaseKey >= 394254)
+                return "4.6.1";
+            if (releaseKey >= 393295)
+                return "4.6";
+            if (releaseKey >= 379893)
+                return "4.5.2";
+            if (releaseKey >= 378675)
+                return "4.5.1";
+            if (releaseKey >= 378389)
+                return "4.5";
+            // This code should never execute. A non-null release key should mean
+            // that 4.5 or later is installed.
+            return "No 4.5 or later version detected";
+        }
+
+        private static void SetHighestTlsVersion()
+        {
+            try
+            { 
+                //Setting TLS to 1.3
+                Console.WriteLine("Setting TLS to 1.3 ...");
+                ServicePointManager.SecurityProtocol = (SecurityProtocolType)12288
+                                                     | (SecurityProtocolType)3072
+                                                     | (SecurityProtocolType)768
+                                                     | SecurityProtocolType.Tls;
+            }
+            catch (NotSupportedException)
+            {
+                try
+                {
+                    //Setting TLS to 1.2
+                    Console.WriteLine("Setting TLS to 1.2 ...");
+                    ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072
+                                                         | (SecurityProtocolType)768
+                                                         | SecurityProtocolType.Tls;
+                }
+                catch (NotSupportedException)
+                {
+                    try
+                    {
+                        //Setting TLS to 1.1
+                        Console.WriteLine("Setting TLS to 1.1 ...");
+                        ServicePointManager.SecurityProtocol = (SecurityProtocolType)768
+                                                             | SecurityProtocolType.Tls;
+                    }
+                    catch (NotSupportedException)
+                    {
+                        //Setting TLS to 1.0
+                        Console.WriteLine("Setting TLS to 1.0 ...");
+                        ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls;
+                    }
+                }
+            }
         }
 
         static void MainImplementation()
-        {            
+        {
             var receiver = new MessageReceiver(settings);
             var sender = new MessageSender(settings);
 
@@ -79,12 +169,12 @@ namespace AmqpTestConsole
 
         private static void StartMessageSenders(MessageSender sender)
         {
-            sender.Start(settings.Address);            
+            sender.Start(settings.Address);
         }
 
         private static void StartMessagePumps(MessageReceiver receiver)
         {
-            receiver.Start(settings.Address, ProcessMessage);            
+            receiver.Start(settings.Address, ProcessMessage);
         }
 
         private static async Task ProcessMessage(Message message)
